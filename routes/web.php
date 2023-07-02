7b79c5d140bfe10cbc83ee0348729284334a994f
Samuel Anselmo <samuelanselmo69@gmail.com>
2023-07-02 11:46:24 -0300

    Models

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Admin\{SupportController};
+use App\Http\Controllers\Site\SiteController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('supports', [SupportController::class, 'index'])->name('index.supports');
+
+Route::get('/contato', [SiteController::class, 'contact']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
