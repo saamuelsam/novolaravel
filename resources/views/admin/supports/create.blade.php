9504ff6731f60c076fe495cf0ce32c33fea9fd9f
Your Name <samuelanselmo69@gmail.com>
2023-07-11 11:28:36 -0300

    error

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -6,7 +6,10 @@
 
 @if($errors->any()) <!-- o any verifica se tem algum erro -->
   @foreach($errors->all() as $error)
+  <div class="alert alert-danger alert-dismissible" role="alert">
   {{$error}}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
   @endforeach
 @endif
 
